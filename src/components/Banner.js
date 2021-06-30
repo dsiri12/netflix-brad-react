8fc89ccae0f20fdf9a5118bd2b080559caa7ae55
Damien Siri <dsiri12@gmail.com>
2021-06-30 15:29:27 -0400

    banner update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className="showcase-top">
+    <div className="showcase-content">
       <h1>See what's next</h1>
       <p>Watch anywhere. Cancel anytime</p>
-      <a href="#" className="btn btn-xl"></a>
+      <a href="#" className="btn btn-xl">
+        Watch Free For 30 Days <i className="fas fa-chevron-right btn-icon"></i>
+      </a>
     </div>
   )
 }
